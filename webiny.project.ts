ec2e851916223da3561dcec6fd6c465e5b4fe1ba
Kevin Wang <kevin.wang.az@gmail.com>
2025-05-08 20:59:39 -0500

    feat: init starter project

---
+++ webiny.project.ts
@@ -0,0 +1,39 @@
+import cliWorkspaces from "@webiny/cli-plugin-workspaces";
+import cliPulumiDeploy from "@webiny/cli-plugin-deploy-pulumi";
+import cliAwsTemplate from "@webiny/cwp-template-aws/cli";
+import cliExtensions from "@webiny/cli-plugin-extensions";
+
+// Scaffolds.
+import cliScaffold from "@webiny/cli-plugin-scaffold";
+import cliScaffoldExtendGraphQlApi from "@webiny/cli-plugin-scaffold-graphql-service";
+import cliScaffoldAdminModule from "@webiny/cli-plugin-scaffold-admin-app-module";
+import cliScaffoldExtensions from "@webiny/cli-plugin-scaffold-extensions";
+import cliScaffoldWorkspaces from "@webiny/cli-plugin-scaffold-workspaces";
+import cliScaffoldCiCd from "@webiny/cli-plugin-scaffold-ci";
+
+export default {
+    template: "@webiny/cwp-template-aws@5.42.3",
+    name: "webiny-cms-starter",
+    cli: {
+        plugins: [
+            cliWorkspaces(),
+            cliPulumiDeploy(),
+            cliAwsTemplate(),
+            cliExtensions(),
+
+            // Scaffolds.
+            cliScaffold(),
+            cliScaffoldExtendGraphQlApi(),
+            cliScaffoldAdminModule(),
+            cliScaffoldExtensions(),
+            cliScaffoldWorkspaces(),
+            cliScaffoldCiCd()
+        ]
+    },
+    appAliases: {
+        core: "apps/core",
+        api: "apps/api",
+        admin: "apps/admin",
+        website: "apps/website"
+    }
+};
